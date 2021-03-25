ffc5cdecac6cdd570e9e1fbffacebdee99b839d9
PhucDuong-SSS <phucdnDev@gmail.com>
2021-03-25 09:11:36 +0700

    implement buyer controller

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerController extends Controller
@@ -14,7 +15,8 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()->json(['data' => $buyers],200);
     }
 
     /**
@@ -46,7 +48,8 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $buyer],200);
     }
 
     /**
